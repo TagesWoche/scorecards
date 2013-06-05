b0b0f1eebb793ba50c13f1f76b1bd3cb205f44e6
David Bauer <info@davidbauer.ch>
2013-06-05 16:22:58 +0200

    Minor adjustments

--- js/formcurve.js
+++ js/formcurve.js
@@ -39,7 +39,7 @@
     legend = svg_container.append('g').attr('class', 'legend');
     legend.append('path').attr('d', "M 5 25 L 5 30 L 70 30 L 70 35 L 80 27.5 L 70 20 L 70 25 L 5 25").attr('fill', '#777').attr('fill-opacity', .7);
     legend.append('text').text('Zeitauswahl').attr('transform', 'translate(5, 15)');
-    legend.append('text').text('Spielerbewertung').attr('transform', 'translate(0, 80)');
+    legend.append('text').text('Note').attr('transform', 'translate(0, 80)');
     return {
       sanitizeData: function(data) {
         var grade, index, player, sanitizedData;
@@ -174,7 +174,7 @@
             path = "M " + (x(d.date)) + " " + (y(d.averageGrade)) + " L " + (x(d.date)) + " " + (y(d.grade) - 15) + " L " + (x(d.date) + 5) + " " + (y(d.grade) - 23) + " L " + (x(d.date) - 5) + " " + (y(d.grade) - 23) + " L " + (x(d.date)) + " " + (y(d.grade) - 15);
             focus.append('path').attr('d', path).attr('stroke', 'red');
           }
-          text = "Gegner: " + d.opponent + "<br/> Note: " + d.grade + ", \n\n<b>+" + (Math.floor((d.averageGrade - d.grade) * 10) / 10) + "</b> gegenüber Team-Schnitt";
+          text = "Gegner: " + d.opponent + "<br/> Note: " + d.grade + ", \n\n<b>-" + (Math.floor((d.averageGrade - d.grade) * 10) / 10) + "</b> gegenüber Team-Schnitt";
           tooltipY = -30;
         }
         tooltip.transition().duration(200).style('opacity', .9);
